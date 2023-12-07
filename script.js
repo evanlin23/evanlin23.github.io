57eb91ae7811c82a68eeb2c0e6addf685b7ef503
evanlin23 <evanlin366@gmail.com>
2023-12-07 11:21:55 -0600

    Unhide js

--- script.js
+++ script.js
@@ -1,1 +1,119 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 a=t;(7(1f,R){2 8=t,v=1f();1Y(!![]){1Z{2 S=9(8(20))/i*(9(8(21))/22)+9(8(23))/24*(9(8(25))/1X)+-9(8(26))/28+9(8(29))/2a+-9(8(2b))/2c+9(8(1V))/2e*(9(8(2f))/2g)+9(8(27))/1W*(-9(8(1T))/1R);11(S===R)1M;12 v[\'M\'](v[\'N\']())}1L(1K){v[\'M\'](v[\'N\']())}}}(p,1S));7 t(1a,13){2 Q=p();x t=7(A,1O){A=A-U;10 1c=Q[A];x 1c},t(1a,13)}2 K={\'s\':[\'1Q\',\'2h\',\'2d\\2j\\2G\',\'2I\\2J\',a(2K),a(2L),\'2M\'],\'u\':l,\'r\':![],\'4\':\'\',\'y\':15,\'L\':7(){2 H=a;!0[\'r\']?0[H(P)]():0[H(2O)]()},\'B\':7(){2 d=a,V=0[d(m)][0[d(g)]];0[d(c)]=V[d(Z)](l,0[d(c)][d(16)]+i),0[d(2Q)]()},\'1I\':7(){2 z=a,1b=0[\'s\'][0[z(g)]];0[\'4\']=1b[z(Z)](l,0[z(c)][\'E\']-i),0[\'n\']()},\'n\':7(){2 6=a;17[6(2U)](\'K\')[6(1e)]=\'<G>\'+0[6(c)]+6(T);10 q=0[6(Y)];0[\'r\']&&(q/=1.5);11(!0[6(I)]&&0[6(c)]===0[6(m)][0[6(g)]])q=2X,0[\'r\']=!![];12 0[6(I)]&&0[\'4\']===\'\'&&(0[6(I)]=![],0[\'u\']=(0[6(g)]+i)%0[6(m)][\'E\'],q=2R);O(0[6(f)][\'2u\'](0),q)}};7 j(2l,2m){2 h=a;0[h(g)]=l,0[h(c)]=\'\',0[h(Y)]=15,0[\'L\']=7(18,C){2 D=h,19=17[D(2n)](\'.\'+18);0[D(m)]=2p[D(2q)](C)?C:[C],0[\'B\'](19)},0[\'B\']=7(X){2 k=h,W=0[k(m)][0[\'u\']];0[k(c)]=W[\'1j\'](l,0[k(c)][k(16)]+i),0[\'n\'](X)},0[\'n\']=7(J){2 e=h;J[e(1e)]=e(U)+0[\'4\']+e(T),0[\'4\']===0[\'s\'][0[e(g)]]&&(0[\'u\']=(0[e(g)]+i)%0[\'s\'][\'E\']),O(()=>0[e(P)](J),0[\'y\'])}}2A[a(2B)]=7(){2 3=a;K[3(f)]();2 1d=o j(\'b-4-1h\',[3(14)]);1d[3(f)](3(2D),[3(14)]);2 1z=o j(3(2H),[3(2i)]);1z[3(f)](\'b-4-1n\',[\'1D,\\1H\\1w\\1i\\w\\1g\\1p\\1t\\1v\']);2 1A=o j(3(1B),[3(2C)]);1A[3(f)](3(1B),[\'1C\\1x\\1o\']);2 1y=o j(3(2z),[3(2y)]);1y[3(f)](\'b-4-1u\',[\'1q\\w\\1r\\1s\\F\']);2 1F=o j(\'b-4-1G\',[\'1k\\F\\1l\\1m\']);1F[3(f)](\'b-4-1G\',[3(2x)])};7 p(){2 1E=[\'b-4-2w\',\'1I\',\'1C\\1x\\1o\',\'y\\2v\',\'2t\',\'2k\\2s\',\'1j\',\'1k\\F\\1l\\1m\',\'B\',\'2r\',\'<G>\',\'b-4-1n\',\'2o\',\'1q\\w\\1r\\1s\\F\',\'E\',\'2E\',\'2F\',\'n\',\'2Y\',\'b-4-1u\',\'4\',\'2W\',\'2V\',\'2T\',\'2S\',\'b-4-1h\',\'2Z\',\'2P\',\'y\',\'L\',\'2N\',\'1P\',\'1N\',\'1U\',\'s\',\'1J\',\'</G>\',\'r\',\'u\',\'1D,\\1H\\1w\\1i\\w\\1g\\1p\\1t\\1v\'];p=7(){x 1E};x p()}',62,186,'this||const|_0x3435d8|text||_0xef1ed4|function|_0xbd6f93|parseInt|_0x31dc38|heading|0x148|_0x2e0568|_0x3f66e6|0x151|0x15a|_0x1b393d|0x1|TypingEffect|_0x3f4b89|0x0|0x156|updateText|new|_0x2bf4|_0x2f76c5|isDeleting|textArray|_0x1052|index|_0x49a83d|x20I|return|speed|_0xa05d8|_0x1052f5|addLetters|_0x54eb4d|_0x235421|length|x20of|span|_0x559af5|0x159|_0x461dd2|typingEffect|type|push|shift|setTimeout|0x164|_0x2bf4cd|_0x56de5b|_0x594319|0x158|0x13e|_0x403446|_0x1a8bb0|_0x3e4559|0x150|0x162|let|if|else|_0x25fa8e|0x155|0x64|0x142|document|_0x4eba71|_0x387de1|_0x1138fa|_0x3c8bcd|_0x1185a4|_0x295788|0x157|_0x2039b3|x27ve|highlight0|x20what|substring|Memories|x20the|x20past|highlight1|x20activities|x20been|Recognitions|x27m|x20proud|x20up|highlight3|x20to|x20is|x20interesting|_0x392971|_0x4c6e72|_0x479c9a|0x15c|Some|Hey|_0x557ee7|_0x3f4842|highlight4|x20this|removeLetters|innerHTML|_0x2889a7|catch|break|1059780ymWXrU|_0x3a6c30|4UZBwNl|student|0xb|0xe9f28|0x14e|Education|0x149|0xa|0x4|while|try|0x165|0x14f|0x2|0x144|0x3|0x153|0x154|0x152|0x5|0x14b|0x6|0x146|0x7|cyber|0x8|0x160|0x9|archer|0x15b|x20security|software|_0x2641ce|_0x3bdc62|0x140|querySelector|Array|0x14a|13JOszUt|x20engineer|9XZqnRR|bind|x20cuber|highlight2|0x163|0x141|0x147|window|0x143|0x15e|0x14d|onload|705477heCJos|x20researcher|0x13f|lego|x20master|0x161|0x15f|brother|10vXJINw|0x15d|191356aLTltR|0x145|0x1f4|getElementById|6369438WsguPc|0x14c|isArray|12985144DIsjlH|0x7d0|682136ezSRSb|31856407XfsDhf'.split('|'),0,{}))
+const typingEffect = {
+  textArray: [
+    "student",
+    "archer",
+    "cyber security researcher",
+    "lego master",
+    "software engineer",
+    "speed cuber",
+    "brother",
+  ],
+
+  index: 0,
+  isDeleting: false,
+  text: "",
+  speed: 100,
+
+  type: function () {
+    if (!this.isDeleting) {
+      this.addLetters();
+    } else {
+      this.removeLetters();
+    }
+  },
+
+  addLetters: function () {
+    const currentText = this.textArray[this.index];
+    this.text = currentText.substring(0, this.text.length + 1);
+    this.updateText();
+  },
+
+  removeLetters: function () {
+    const currentText = this.textArray[this.index];
+    this.text = currentText.substring(0, this.text.length - 1);
+    this.updateText();
+  },
+
+  updateText: function () {
+    document.getElementById(
+      "typingEffect"
+    ).innerHTML = `<span>${this.text}</span>`;
+    let delta = this.speed;
+    if (this.isDeleting) {
+      delta /= 1.5;
+    }
+    if (!this.isDeleting && this.text === this.textArray[this.index]) {
+      delta = 2000;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.text === "") {
+      this.isDeleting = false;
+      this.index = (this.index + 1) % this.textArray.length;
+      delta = 500;
+    }
+    setTimeout(this.type.bind(this), delta);
+  },
+};
+
+function TypingEffect(className, heading) {
+  this.index = 0;
+  this.text = "";
+  this.speed = 100;
+
+  this.type = function (className, heading) {
+    const headingText = document.querySelector(`.${className}`);
+    this.textArray = Array.isArray(heading) ? heading : [heading];
+
+    this.addLetters(headingText);
+  };
+
+  this.addLetters = function (headingText) {
+    const currentText = this.textArray[this.index];
+    this.text = currentText.substring(0, this.text.length + 1);
+    this.updateText(headingText);
+  };
+
+  this.updateText = function (headingText) {
+    headingText.innerHTML = `<span>${this.text}</span>`;
+    if (this.text === this.textArray[this.index]) {
+      this.index = (this.index + 1) % this.textArray.length;
+    }
+    setTimeout(() => this.addLetters(headingText), this.speed);
+  };
+}
+
+window.onload = function () {
+  typingEffect.type();
+
+  const typingEffectHeading1 = new TypingEffect("heading-text-highlight0", [
+    "Education",
+  ]);
+  typingEffectHeading1.type("heading-text-highlight0", ["Education"]);
+
+  const typingEffectHeading2 = new TypingEffect("heading-text-highlight1", [
+    "Hey, this is what I've been up to",
+  ]);
+  typingEffectHeading2.type("heading-text-highlight1", [
+    "Hey, this is what I've been up to",
+  ]);
+
+  const typingEffectHeading3 = new TypingEffect("heading-text-highlight2", [
+    "Some interesting activities",
+  ]);
+  typingEffectHeading3.type("heading-text-highlight2", [
+    "Some interesting activities",
+  ]);
+
+  const typingEffectHeading4 = new TypingEffect("heading-text-highlight3", [
+    "Recognitions I'm proud of",
+  ]);
+  typingEffectHeading4.type("heading-text-highlight3", [
+    "Recognitions I'm proud of",
+  ]);
+
+  const typingEffectHeading5 = new TypingEffect("heading-text-highlight4", [
+    "Memories of the past",
+  ]);
+  typingEffectHeading5.type("heading-text-highlight4", [
+    "Memories of the past",
+  ]);
+};
